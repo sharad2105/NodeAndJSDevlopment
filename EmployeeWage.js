01d9324b515d5473f711cd4031c841d70d8c0f8c
Admin <patilsharad2000@gmail.co>
2021-03-15 11:43:36 +0530

    UC-1 Check Employee is Absent

---
+++ EmployeeWage.js
@@ -0,0 +1,7 @@
+const IS_ABSENT = 0;
+let empCheck = Math.floor(Math.random() * 10) % 2;
+if (empCheck == IS_ABSENT) {
+    console.log("Employee Is Absent");
+} else {
+    console.log("Employee is Present");
+}
---
+++ practice.js
@@ -0,0 +1,21 @@
+< !DOCTYPE html >
+    <
+    html >
+    <
+    head >
+    <
+    script >
+    function myFunction() {
+        document.getElementById("demo").innerHTML = "paragraph changed."
+
+    } <
+    /script> <
+    /head> <
+    body >
+    <
+    h1 > Web Page < /h1> <
+    p id = "demo" > A Paragraph < /p> <
+    Button type = "button"
+onclick = "myFunction()" > Try it < /Button> <
+    /body> <
+    /html>
